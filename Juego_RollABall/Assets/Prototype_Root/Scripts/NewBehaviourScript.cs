434c9a390d8a13fdb6c1dea13cd9ff45f7ed1c9b
Zhey10 <Zhey10@users.noreply.github.com>
2024-11-22 15:33:33 +0100

    nextlevel

--- Juego_RollABall/Assets/Prototype_Root/Scripts/NewBehaviourScript.cs
+++ Juego_RollABall/Assets/Prototype_Root/Scripts/NewBehaviourScript.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public string sceneName;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,12 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
     }
 }
