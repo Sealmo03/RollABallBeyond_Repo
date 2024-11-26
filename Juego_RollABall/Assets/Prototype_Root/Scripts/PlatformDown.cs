23090c929d05f044314fd2a700b475daf8d56b4b
Zhey10 <Zhey10@users.noreply.github.com>
2024-11-26 11:50:20 +0100

    scripts

---
+++ Juego_RollABall/Assets/Prototype_Root/Scripts/PlatformDown.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDown : MonoBehaviour
+{
+
+    public float fallSpeed;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+        }
+    }
+}
