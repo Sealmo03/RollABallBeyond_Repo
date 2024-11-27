fd4303d63684126c365e8d0cc5643e408d12dc56
Zhey10 <Zhey10@users.noreply.github.com>
2024-11-27 01:42:45 +0100

    primernivelacabado

--- Juego_RollABall/Assets/Scripts/Player/PlayerMovement.cs
+++ Juego_RollABall/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,45 +1,103 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
+    public Rigidbody playerRb;
 
-    [Header("Public References")]
-    public Rigidbody playerRb; //Variable para referenciar el rb del jugador, y asi modificarlo cuando quiera en el codigo
 
-    [Header("Movement Variables")]
-    public float speed;
-    private float horInput; // Almac�n del vector X del input
-    private float verInput; // Almac�n del vector Y del input (se lo pasar� al eje z)
 
-    [Header("Jump Variables")]
     public float jumpForce;
+    public float airMultiplier;
+
+    [HideInInspector] public float walkSpeed;
+    [HideInInspector] public float sprintSpeed;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
     public bool isGrounded = true;
+    public bool grounded = true;
 
+    public Transform orientation;
 
+    float horizontalInput;
+    float verticalInput;
 
-    // Start is called before the first frame update
-    void Start()
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    [SerializeField] Transform cameraFollowTarget;
+
+    private void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        //Conectar las variables float al input de teclado
-        horInput = Input.GetAxis("Horizontal");
-        verInput = Input.GetAxis("Vertical");
+
+        Jump();
+        MyInput();
+        SpeedControl();
 
     }
 
     private void FixedUpdate()
     {
-        Movement();
-        Jump();
+        MovePlayer();
+    }
 
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+    
+       
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if(grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded == true)
+            {
+                isGrounded = false;
+                playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -47,21 +105,22 @@ public class PlayerMovement : MonoBehaviour
         {
             isGrounded = true;
         }
-
     }
-    void Movement()
+    private void OnCollisionStay(Collision collision)
     {
-        //Velocidad del rigidbody = Vector3 (Movimiento x, Movimiento Y, Movimiento Z)
-        playerRb.velocity = new Vector3(horInput * speed, playerRb.velocity.y, verInput * speed);
-    }
-
-    void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = false;
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = true;
         }
+    }
 
+
+    private void LateUpdate()
+    {
+        CameraRotation();
+    }
+    void CameraRotation()
+    {
+        cameraFollowTarget.rotation = Quaternion.identity;
     }
 }
