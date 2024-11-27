3242609354c4a74d8a40f1f326d47c64446c6d5d
Sealmo03 <157643554+Sealmo03@users.noreply.github.com>
2024-11-27 23:46:57 +0100

    commit final

--- Juego_RollABall/Assets/Scripts/Texto.cs
+++ Juego_RollABall/Assets/Scripts/Texto.cs
@@ -8,7 +8,7 @@ public class Texto : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke(nameof(ApagarPanel), 20f);
+        Invoke(nameof(ApagarPanel), 15f);
     }
 
     // Update is called once per frame
